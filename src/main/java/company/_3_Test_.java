2642014fdc95487ef6d957560f5a14e00cf23367
AlexandrNedilko <aleksandr_ned@ukr.net>
2019-08-09 18:31:25 +0300

    Add les-018

--- src/main/java/company/_3_Test_.java
+++ src/main/java/company/_3_Test_.java
@@ -4,64 +4,62 @@ package company;
 
 import java.io.File;
 import java.io.IOException;
+
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
 
 public class _3_Test_ {
-    static ArrayList<File> scanDir(String dir, ArrayList<File> list) throws IOException {
+    public static void main(String[] args) throws Exception {
+        ArrayList<File> list = new ArrayList<File>();
+        String str = "C:\\Java Developer\\lesson-018\\tepmsrc";
+
+        list = scanDir(str, list);
+
+        String text = getTextfromFilesDir(list);
+         writeTextToFile("C:\\Java Developer\\lesson-018\\copyNameFilse.txt", text);
+    }
+
+    private static String getTextfromFilesDir(ArrayList<File> list) throws IOException {
+        StringBuilder stringBuilder =new StringBuilder();
+        String text;
+          for (File f: list ) {
+             stringBuilder.append(f.getCanonicalPath()+ "\r\n");
+             stringBuilder.append( f.getName()+ "\r\n");
+             stringBuilder.append( f.lastModified()+ "\r\n");
+        }
+        text= stringBuilder.toString();
+        return  text;
+    }
+
+    static ArrayList<File> scanDir(String dir, ArrayList<File> arr) throws IOException {
+
         File f = new File(dir);
         File[] filesList = f.listFiles();
 
         if (filesList == null)
-            return list;
+            return arr;
 
         for (File file : filesList) {
             if (file.isFile()) {
-                list.add(file);
+                arr.add(file);
             }
             if (file.isDirectory()) {
-                list.add(file);
-                scanDir(file.getCanonicalPath(), list);
+                arr.add(file);
+                scanDir(file.getCanonicalPath(), arr);
             }
         }
-        return list;
+        return arr;
     }
 
-    static void copyFile(String src, String dst) throws IOException {
-        byte[] buff = new byte[1024];
-        int r = 0;
-
-
-        RandomAccessFile in = new RandomAccessFile(src, "r");
-        RandomAccessFile out = new RandomAccessFile(dst, "rw");
-
-        do {
-            r = in.read(buff, 0, buff.length);
-            if (r > 0) {
-                out.write(buff, 0, r);
-            }
-        } while (r > 0);
-
-
-    }
-
-    static void copyAllInDir(String src, String dst) throws IOException {
-        ArrayList<File> list = new ArrayList<File>();
-        StringBuilder sb = new StringBuilder();
-
-        list = scanDir(src, list);
-
-        for (File f : list) {
-            sb.append(f.getCanonicalPath());
-            sb.replace(0, src.length(), dst);
-
-            if (f.isDirectory()) {
-                new File(sb.toString()).mkdirs();
-            }
-            if (f.isFile()) {
-                copyFile(f.getCanonicalPath(), sb.toString());
-            }
-            sb.delete(0, sb.length());
+    static void writeTextToFile(String file, String text) throws  IOException {
+        RandomAccessFile f = new RandomAccessFile(file, "rw");
+        try {
+            f.setLength(0);
+            byte[] buf = text.getBytes();
+            f.write(buf);
+        }finally {
+            f.close();
         }
     }
+
 }
